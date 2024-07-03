ce0e4bad0120765625a998fff16a2f990033184e
Rashed Rana <75809484+rashedrana@users.noreply.github.com>
2024-07-03 20:35:35 +0200

    Update analysis.py

--- project/analysis.py
+++ project/analysis.py
@@ -7,7 +7,6 @@ dataP = pipeline.main()
 
 conn = sqlite3.connect(dataP[0])
 
-# Query to select all data from the CO2Emissions_FossilFuel table
 data_query = f'''
 SELECT	T1.Country_C [Country],
 		T1.ISO3 [ISO3],
