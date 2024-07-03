82595caf2e426bcae0d4dbca4d62f6ec9840d976
Rashed Rana <75809484+rashedrana@users.noreply.github.com>
2024-07-03 20:32:06 +0200

    Update analysis.py

--- project/analysis.py
+++ project/analysis.py
@@ -74,7 +74,6 @@ plt.show()
 countries = ["TUR", "LTU", "ARG", "IND", "LUT", "JPN"]
 filtered_data = data[data['ISO3'].isin(countries)]
 
-
 years = ['F2015_C', 'F2016_C', 'F2017_C', 'F2018_C', 'F2019_C', 'F2020_C', 'F2021_C']
 
 # Convert wide format data to long format for easier manipulation
